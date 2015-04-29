5a194b53247b80b4894377b1070a271b58d8c537
jackXiaoFeng <625530207@qq.com>
2015-04-29 13:32:38 +0800

    Initial Commit

---
+++ github test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github test
+//
+//  Created by yangkaifeng on 15/4/29.
+//  Copyright (c) 2015年 yangkaifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
